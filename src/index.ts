ed1056108ae85189e38944cd856b07caf42b34cb
Ísak Arnar Kolbeins <isakkolbeins@gmail.com>
2024-02-26 19:46:44 +0100

    working ?

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ import pg from "pg";
 
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
-const pool = new pg.Pool();
+// const pool = new pg.Pool();
+// The secret connection string you copied earlier
+const connectionString = process.env.DATABASE_URL;
+  //"postgresql://postgres:5C1gbdGF2DdgFCEGAff46CBge2DfC1*6@postgres.railway.internal:5432/railway";
+  // '${{ DATABASE_URL }}';
+const pool = new pg.Pool({
+  connectionString,
+});
 
 const app = express();
 const port = process.env.PORT || 3333;
