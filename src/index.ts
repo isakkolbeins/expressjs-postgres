41df71a54d4df08d3d1a3ffb26d6df1d99a96214
Ísak Arnar Kolbeins <isakkolbeins@gmail.com>
2024-02-26 15:02:39 +0100

    db check

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,20 @@ const pool = new pg.Pool();
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+// app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+// app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
   // const { rows } = await pool.query("SELECT NOW()");
+  res.send(`Hello, World! The time from the DB is simple return`);
+});
+
+app.get("/ads", async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM ads");
   res.send(`Hello, World! The time from the DB is simple return`);
 });
 
